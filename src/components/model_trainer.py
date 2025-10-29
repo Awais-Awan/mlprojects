e89d9e704ba9e1f39ed7b37b756e669287fe6883
Awais-Awan <awanawais400@gmail.com>
2025-10-29 22:43:01 +0500

    Hyperparameter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -5,7 +5,7 @@ from  dataclasses import dataclass
 from catboost import CatBoostRegressor
 from sklearn.ensemble import (
     AdaBoostRegressor,
-    HistGradientBoostingRegressor,
+    GradientBoostingRegressor,
     RandomForestRegressor,
 )
 from sklearn.linear_model import LinearRegression
@@ -42,15 +42,75 @@ class ModelTrainer:
             models = {
                 "Random Forest" : RandomForestRegressor(),
                 "Decision Tree" : DecisionTreeRegressor(),
-                "Gradient Boosting" : HistGradientBoostingRegressor(),
+                "Gradient Boosting" : GradientBoostingRegressor(),
                 "Linear Regression" : LinearRegression(),
-                "K-Neighbors Classifier" : KNeighborsRegressor(),
-                "XGBClassifier" : XGBRegressor(),
-                "CatBoosting Classifier" : CatBoostRegressor(),
-                "AdaBoost Classifier" : AdaBoostRegressor(),
+                "K-Neighbour Regressor" : KNeighborsRegressor(),
+                "XGBRegressor" : XGBRegressor(),
+                "CatBoosting Regressor" : CatBoostRegressor(),
+                "AdaBoost Regressor" : AdaBoostRegressor(),
             }
 
-            model_report: dict= evaluate_model(X_train,Y_train,X_test,Y_test, models=models)
+            params = {
+                "Decision Tree": {
+                    'criterion': ['squared_error', 'friedman_mse'],
+                    # 'splitter': ['best', 'random'],
+                    'max_depth': [None, 10, 20],
+                    # 'min_samples_split': [2, 5, 10],
+                    # 'min_samples_leaf': [1, 2, 4],
+                },
+
+                "Random Forest": {
+                    'n_estimators': [50, 100],
+                    # 'criterion': ['squared_error', 'absolute_error'],
+                    'max_depth': [10, 20],
+                    # 'min_samples_split': [2, 5],
+                    # 'min_samples_leaf': [1, 2],
+                    # 'max_features': ['sqrt', 'log2'],
+                    # 'bootstrap': [True, False],
+                },
+
+                "Gradient Boosting": {
+                    'n_estimators': [50, 100],
+                    'learning_rate': [0.05, 0.1],
+                    # 'subsample': [0.8, 1.0],
+                    'max_depth': [3, 5],
+                    # 'min_samples_split': [2, 5],
+                    # 'min_samples_leaf': [1, 2],
+                },
+
+                "Linear Regression": {},
+
+                "K-Neighbour Regressor": {
+                    'n_neighbors': [3, 5, 7],
+                    # 'weights': ['uniform', 'distance'],
+                    # 'algorithm': ['auto', 'ball_tree'],
+                },
+
+                "XGBRegressor": {
+                    'learning_rate': [0.05, 0.1],
+                    'n_estimators': [50, 100],
+                    'max_depth': [3, 5],
+                    # 'subsample': [0.8, 1.0],
+                    # 'colsample_bytree': [0.8, 1.0],
+                    # 'gamma': [0, 0.1],
+                },
+
+                "CatBoosting Regressor": {
+                    'depth': [6, 8],
+                    'learning_rate': [0.05, 0.1],
+                    'iterations': [100, 200],
+                    # 'l2_leaf_reg': [3, 5],
+                },
+
+                "AdaBoost Regressor": {
+                    'n_estimators': [50, 100],
+                    'learning_rate': [0.05, 0.1],
+                    # 'loss': ['linear', 'square', 'exponential'],
+                }
+            }
+
+
+            model_report: dict= evaluate_model(X_train,Y_train,X_test,Y_test, models=models, param = params)
 
 
 
@@ -71,7 +131,6 @@ class ModelTrainer:
 
 
             predicted = best_model.predict(X_test)
-            
             r2_Score = r2_score(Y_test,predicted)
             return r2_Score
 
